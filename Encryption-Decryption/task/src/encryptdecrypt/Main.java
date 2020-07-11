3838f9e997191557b44f54b2742b1779bcab708a
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2020-07-11 16:33:02 +0700

    Finish Stage 2/6

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,46 +1,24 @@
 package encryptdecrypt;
 
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-    static HashMap<Character, Character> hm = new HashMap<>(25){
-        {
-            put('a','z');
-            put('b','y');
-            put('c','x');
-            put('d','w');
-            put('e','v');
-            put('f','u');
-            put('g','t');
-            put('h','s');
-            put('i','r');
-            put('j','q');
-            put('k','p');
-            put('l','o');
-            put('m','n');
-            put('n','m');
-            put('o','l');
-            put('p','k');
-            put('q','j');
-            put('r','i');
-            put('s','h');
-            put('t','g');
-            put('u','f');
-            put('v','e');
-            put('w','d');
-            put('x','c');
-            put('y','b');
-            put('z','a');
-        }
-    };
-    public static void main(String[] args) {
-        String input = "we found a cdtreasure!";
-        for (char i: input.toCharArray()){
-            if (!hm.containsKey(i)) {
-                System.out.print(i);
-                continue;
-            }
-            System.out.print(hm.get(i));
-        }
-    }
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String input = in.nextLine();
+		int shift = in.nextInt();
+		for (char i : input.toCharArray()) {
+			if (!Character.isLetter(i)) {
+				System.out.print(i);
+				continue;
+			}
+			int ascii = i + shift;
+			if (ascii > 97 + 26) {
+				ascii = 97 + (ascii % (97 + 26));
+			}
+			char x = (char) (ascii);
+			System.out.print(x);
+		}
+	}
 }
